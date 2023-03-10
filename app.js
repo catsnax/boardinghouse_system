a8a44f6c13c2b0389c662964b83b674409983a0d
catsnax <aaron.justin.macias@gmail.com>
2023-03-10 16:50:34 +0800

    added new columns to table. page now redirects to another page to edit records. also added some styles to the table.

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 const Boarder = require('./models/boarder');
+
 const { redirect } = require('express/lib/response');
 
 const app = express();
+
+//db URI is from mongoose db. substitute  username and password in template provided
 const dbURI = 'mongodb+srv://CatSnaxx:Aaronisreal4@boarderdatabase.di9lyeh.mongodb.net/boarders?retryWrites=true&w=majority';
 
 app.set('view engine', 'ejs');
@@ -21,6 +24,7 @@ mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
     console.log(err);
 })
 
+
 app.get('/', (req, res) =>{
     Boarder.find()
     .then((result) =>{
@@ -31,41 +35,58 @@ app.get('/', (req, res) =>{
     })
 })
 
-app.post('/', (req, res) =>{
-    const id = req.body.index;
-    Boarder.findById(id).then((result) =>{
-        result.boarderRentStatus = req.body.payStatus;
-        result.save().then(() =>{}).catch((err) => {console.log(err)})
-        res.redirect('/');
-    }
-    ).catch((err) => {
-        console.log(err);
-    })
-
-
-    
-})
-
-
-app.get('/create', (req, res) =>{
-    res.render('create');
-})
-
+//takes in the data from html forms and creates a new instance of Boarder which is new object of boarder in data using schema
 app.post('/create', (req, res) =>{
     const newBoarder = new Boarder({
         boarderName : req.body.Name,
         boarderContact: req.body.Contact,
         boarderRent: req.body.rentFee,
         boarderRentDue: req.body.rentDue,
-        boarderRentStatus: "Not Paid"
-});
+        boarderRentStatus: "Not Paid",
+        boarderWater: 0,
+        boarderWaterStatus: "Not Paid",
+        boarderElect: 0,
+        boarderElectStatus: "Not Paid"
+    });
 
-newBoarder.save()
-.then((result) =>{
-    console.log(result);
+    newBoarder.save()
+    .then((result) =>{
+        console.log(result);
+    })
+    .catch((err) => console.log(err));
+    res.redirect('/');
 })
-.catch((err) => console.log(err));
 
-res.redirect('/');
+app.get('/edit', (req, res) =>{
+    Boarder.find()
+    .then((result) =>{
+        res.render('edit', {data : result});
+    })
+    .catch((err) =>{
+        console.log(err);
+    })
+    
 })
 
+app.post('/edit', (req, res) =>{
+    const id = req.body.index;
+    Boarder.findById(id).then((result) =>{
+        console.log(req.body);
+        
+        result.boarderRentStatus = req.body.payStatus;
+        result.boarderWater = req.body.water;
+        result.boarderWaterStatus = req.body.waterStatus
+        result.boarderElect = req.body.elect;
+        result.boarderElectStatus = req.body.electStatus
+
+        result.save().then(() =>{res.redirect('/edit');}).catch((err) => {console.log(err)})
+        
+    }
+    ).catch((err) => {
+        console.log(err);
+    }) 
+})
+
+app.get('/create', (req, res) =>{
+    res.render('create');
+})
